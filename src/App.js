48027afb680d786fb025f62a98c77352cdc23f19
NAAborley <jemisco221@gmail.com>
2022-07-12 15:06:08 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ClassCounter from './component/ClassCounter';
+import FunctionCounter from './component/FunctionCounter';
+
+function App() {
+  return (
+    <div>
+      <ClassCounter />
+      <FunctionCounter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/ClassCounter.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class ClassCounter extends Component {
+  constructor(props) {
+    super();
+    this.state = { count: 0 };
+  }
+  handleIncrease = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleDecrease = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <h3>count: {this.state.count}</h3>
+        <button onClick={this.handleIncrease}>increase</button>
+        <button onClick={this.handleDecrease}>decrase</button>
+      </div>
+    );
+  }
+}
+
+export default ClassCounter;
